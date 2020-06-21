df9ec77f104854f55596103b3767916f4a81557f
Muhammad Yasir <taimoormuhammad009@gmail.com>
2020-06-21 17:08:39 +0500

    Homepage

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { HomePage } from "./homepage";
+
+function App() {
+  return <HomePage />;
+}
+
+export default App;
