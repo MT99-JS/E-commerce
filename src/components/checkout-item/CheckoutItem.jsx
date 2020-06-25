630b612158917687b09d62496ad0212288bcc53e
Muhammad Yasir <taimoormuhammad009@gmail.com>
2020-06-25 21:38:38 +0500

    Added increase and decrease functionality in checkout page for quantity of product

--- src/components/checkout-item/CheckoutItem.jsx
+++ src/components/checkout-item/CheckoutItem.jsx
@@ -1,15 +1,36 @@
 import React from "react";
 import "./checkoutitem.scss";
-const CheckoutItem = ({ cartItem: { name, quantity, price, imageUrl } }) => (
-  <div className="checkout-item">
-    <div className="image-container">
-      <img src={imageUrl} alt="item" />
+import { connect } from "react-redux";
+import { clearItem, addItem, removeItem } from "../../redux/cart/cart-action";
+const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
+  const { name, quantity, price, imageUrl } = cartItem;
+  return (
+    <div className="checkout-item">
+      <div className="image-container">
+        <img src={imageUrl} alt="item" />
+      </div>
+      <span className="name">{name}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={() => removeItem(cartItem)}>
+          &#10094;
+        </div>
+        <span className="value">{quantity}</span>
+        <div className="arrow" onClick={() => addItem(cartItem)}>
+          &#10095;
+        </div>
+      </span>
+      <span className="price">{price}</span>
+      <span className="remove-button" onClick={() => clearItem(cartItem)}>
+        &#10005;
+      </span>
     </div>
-    <span className="name">{name}</span>
-    <span className="quantity">{quantity}</span>
-    <span className="price">{price}</span>
-    <span className="remove-button">&#10005;</span>
-  </div>
-);
+  );
+};
 
-export default CheckoutItem;
+const mapDispatchToProps = (dispatch) => ({
+  clearItem: (item) => dispatch(clearItem(item)),
+  addItem: (item) => dispatch(addItem(item)),
+  removeItem: (item) => dispatch(removeItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
--- src/redux/cart/card-actiontype.js
+++ src/redux/cart/card-actiontype.js
@@ -1,6 +1,8 @@
 const CartActionTypes = {
   TOGGLE_CART_HIDDEN: "TOGGLE_CART_HIDDEN",
   ADD_ITEM: "ADD_ITEM",
+  REMOVE_ITEM: "REMOVE_ITEM",
+  CLEAR_CART_ITEM: "CLEAR_CART_ITEM",
 };
 
 export default CartActionTypes;
